d587a3299f616a9714cf290d521907e70b92d2f4
Andrey  ARISTOV <andrey.aristov@pasteur.fr>
2022-05-12 11:35:19 +0200

    Fix binning
    The code was starting with binning 2x2, fix that.
    Plus dynamically update .attrs

--- src/zarr_tools/convert.py
+++ src/zarr_tools/convert.py
@@ -21,34 +21,38 @@ def nd2_to_zarr(path_nd2, out=None, steps=6, dry_run=False):
     )
 
 
-def to_zarr(dask_input:da.Array, path:str=None, steps=3, channel_axis=1, dry_run=False):
+def to_zarr(dask_input:da.Array, path:str=None, steps=3, channel_axis=1, dry_run=False, **kwargs):
     store = zarr.DirectoryStore(baseurl := path.replace('.nd2', '.zarr') if path is None else path)
     grp = zarr.group(store)
-    grp.attrs['multiscales'] = {
-        "multiscales": [
-            {
-                "datasets": [
-                    {
-                        "path": str(i)
-                    } for i in range(steps)
-                ],
-                "name": os.path.basename(baseurl),
-                "type": "nd2",
-                "channel_axis": channel_axis,
-                "version": "0.1"
-            },
-
-        ]
-    }
+    
     print(baseurl)
+    datasets = []
     for i in range(steps):
-        data = dask_input[...,::2,::2]
+        if i==0:
+            data = dask_input
+        else:
+            data = dask_input[...,::2,::2]
+
         try:
             data = data.rechunk()
             print(data.chunksize, data.shape)
             if not dry_run:
                 data.to_zarr(ppp:=os.path.join(baseurl, str(i)))
                 print(f'saved {ppp}')
+                datasets.append({"path": i})
+                grp.attrs['multiscales'] = {
+                    "multiscales": [
+                        {
+                            "datasets": datasets,
+                            "name": os.path.basename(baseurl),
+                            "type": "nd2",
+                            "channel_axis": channel_axis,
+                            "version": "0.1",
+                            **kwargs
+                        },
+
+                    ]
+                }
             else:
                 print(f'dry-run `to save to` {os.path.join(baseurl, str(i))}')
                 
