e6149184685dbecd8e72d41790a876e27d7ece29
Andrey Aristov <aaristov@pasteur.fr>
2022-05-16 14:10:17 +0200

    fix path strings

--- src/zarr_tools/convert.py
+++ src/zarr_tools/convert.py
@@ -36,26 +36,26 @@ def to_zarr(dask_input:da.Array, path:str=None, steps=3, channel_axis=1, dry_run
         try:
             data = data.rechunk()
             print(data.chunksize, data.shape)
-            if not dry_run:
-                data.to_zarr(ppp:=os.path.join(baseurl, str(i)))
-                print(f'saved {ppp}')
-                datasets.append({"path": i})
-                grp.attrs['multiscales'] = {
-                    "multiscales": [
-                        {
-                            "datasets": datasets,
-                            "name": os.path.basename(baseurl),
-                            "type": "nd2",
-                            "channel_axis": channel_axis,
-                            "version": "0.1",
-                            **kwargs
-                        },
-
-                    ]
-                }
-            else:
+            if dry_run:
                 print(f'dry-run `to save to` {os.path.join(baseurl, str(i))}')
-                
+                continue
+            
+            data.to_zarr(ppp:=os.path.join(baseurl, str(i)))
+            print(f'saved {ppp}')
+            datasets.append({"path": str(i)})
+            grp.attrs['multiscales'] = {
+                "multiscales": [
+                    {
+                        "datasets": datasets,
+                        "name": os.path.basename(baseurl),
+                        "type": "nd2",
+                        "channel_axis": channel_axis,
+                        "version": "0.1",
+                        **kwargs
+                    },
+
+                ]
+            }
             dask_input = da.from_zarr(ppp)
         except Exception as e:
             print(e.args)
