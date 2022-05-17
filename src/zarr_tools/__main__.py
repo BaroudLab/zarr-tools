95267652e9f73cf218d8aa45ae0c2aef6e9eeb0e
Andrey Aristov <aaristov@pasteur.fr>
2022-05-17 15:19:31 +0200

    save metadata, add lut limits

--- src/zarr_tools/__main__.py
+++ src/zarr_tools/__main__.py
@@ -16,15 +16,24 @@ def main(nd2_path:str, output:str=None, channel_axis:int=1, steps:int=5, dry_run
     if output is None:
         output = nd2_path.replace('.nd2', '.zarr')
 
+        
+    out = to_zarr(
+        data, 
+        output, 
+        steps=steps, 
+        dry_run=dry_run, 
+        channel_axis=channel_axis, 
+        sizes=d.sizes,
+        lut=get_lut(d))
+    
+    assert os.path.exists(out), "Saving zarr failed..."
+    
     try:
         meta_path = save_metadata(d, output)
         print(f'Saved metadata to {meta_path}')
     except Exception as e:
         print(f'Saving metadata Failed due to {e}')
         
-    out = to_zarr(data, output, steps=steps, dry_run=dry_run, channel_axis=channel_axis, sizes=d.sizes)
-    
-    assert os.path.exists(out), "Saving zarr failed..."
     exit(0)
 
 def save_metadata(data:nd2.ND2File, output:str):
@@ -36,6 +45,20 @@ def save_metadata(data:nd2.ND2File, output:str):
     except TypeError as e:
         raise e
     return meta_path
+
+def get_lut(data:nd2.ND2File):
+    try:
+        meta = data.custom_data
+        return \
+        [
+            [
+                meta['LUTDataV1_0']['LutParam']['CompLutParam'][f'{ch:02d}'][lim][0] \
+                for lim in ['MinSrc','MaxSrc']
+            ] for ch in range(data.sizes['C'])
+        ]
+    except Exception as e:
+        print(f'Unable to get LUT due to {e}')
+        return None
 
 if __name__=="__main__":
     fire.Fire(main)
