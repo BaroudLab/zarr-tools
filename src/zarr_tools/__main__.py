981c03c725b86a446c893ebd60971f5e24d981a3
Andrey Aristov <aaristov@pasteur.fr>
2022-05-17 14:58:42 +0200

    save matadata

--- src/zarr_tools/__main__.py
+++ src/zarr_tools/__main__.py
@@ -1,11 +1,13 @@
 from .convert import to_zarr
 import fire
+import json
 import nd2
 import os
 
-def main(nd2_path:str, output:str=None, channel_axis:int=1, steps:int=6, dry_run=False):
+def main(nd2_path:str, output:str=None, channel_axis:int=1, steps:int=5, dry_run=False):
     data = (d := nd2.ND2File(nd2_path)).to_dask().rechunk()
     print(d.sizes)
+
     try:
         channel_axis = list(d.sizes.keys()).index('C')
     except ValueError:
@@ -13,9 +15,27 @@ def main(nd2_path:str, output:str=None, channel_axis:int=1, steps:int=6, dry_run
 
     if output is None:
         output = nd2_path.replace('.nd2', '.zarr')
+
+    try:
+        meta_path = save_metadata(d, output)
+        print(f'Saved metadata to {meta_path}')
+    except Exception as e:
+        print(f'Saving metadata Failed due to {e}')
+        
     out = to_zarr(data, output, steps=steps, dry_run=dry_run, channel_axis=channel_axis, sizes=d.sizes)
-    assert os.path.exists(out), "Failed..."
+    
+    assert os.path.exists(out), "Saving zarr failed..."
     exit(0)
+
+def save_metadata(data:nd2.ND2File, output:str):
+    try:
+        meta = data.custom_data
+        meta['NDControlV1_0'] = {}
+        with open(meta_path:=os.path.join(output, '.metadata'), 'w') as f:
+            json.dump(meta, f, indent=4)
+    except TypeError as e:
+        raise e
+    return meta_path
 
 if __name__=="__main__":
     fire.Fire(main)
