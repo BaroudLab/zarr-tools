b9d6dfd65085b5ad2017aabee596317d6e2d9e78
Andrey Aristov <aaristov@pasteur.fr>
2022-05-17 15:18:49 +0200

    cleanup, add chunksize

--- src/zarr_tools/convert.py
+++ src/zarr_tools/convert.py
@@ -1,28 +1,16 @@
 import zarr
 import dask.array as da
 import os
-import nd2
 
-def nd2_to_zarr(path_nd2, out=None, steps=6, dry_run=False):
-    '''
-    Converts nd2 to zarr, multiscale
-    '''
-    data = nd2.ND2File(path_nd2)
-    print(sizes := data.sizes)
-    channel_axis = list(sizes.keys()).index('C')
-    out = (path_nd2.replace('.nd2', '.zarr') if out is None else path_nd2)
-    dask_input = data.to_dask()
-    _ = to_zarr(
-        dask_input=dask_input,
-        path=out,
-        steps=steps,
-        channel_axis=channel_axis,
-        dry_run=dry_run
-    )
-
-
-def to_zarr(dask_input:da.Array, path:str=None, steps=3, channel_axis=1, dry_run=False, **kwargs):
-    store = zarr.DirectoryStore(baseurl := path.replace('.nd2', '.zarr') if path is None else path)
+def to_zarr(
+    dask_input:da.Array, 
+    path:str=None, 
+    steps=3, 
+    channel_axis=1, 
+    dry_run=False, 
+    **kwargs
+):
+    store = zarr.DirectoryStore(baseurl := path)
     grp = zarr.group(store)
     
     print(baseurl)
@@ -35,7 +23,7 @@ def to_zarr(dask_input:da.Array, path:str=None, steps=3, channel_axis=1, dry_run
 
         try:
             data = data.rechunk()
-            print(data.chunksize, data.shape)
+            print(data.chunksize, data.shape, data.npartitions)
             if dry_run:
                 print(f'dry-run `to save to` {os.path.join(baseurl, str(i))}')
                 continue
