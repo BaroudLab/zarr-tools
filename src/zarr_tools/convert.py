0e0ae99c6c2fbd76df3966dfefc224fb55b50afd
Andrey Aristov <aaristov@pasteur.fr>
2023-04-03 12:23:58 +0200

    add space

--- src/zarr_tools/convert.py
+++ src/zarr_tools/convert.py
@@ -1,37 +1,39 @@
-import zarr
-import dask.array as da
 import os
 
+import dask.array as da
+import zarr
+
+
 def to_zarr(
-    dask_input:da.Array, 
-    path:str=None, 
-    steps=3, 
-    channel_axis=1, 
-    dry_run=False, 
-    **kwargs
+    dask_input: da.Array,
+    path: str = None,
+    steps=3,
+    channel_axis=1,
+    dry_run=False,
+    **kwargs,
 ):
     store = zarr.DirectoryStore(baseurl := path)
     grp = zarr.group(store)
-    
+
     print(baseurl)
     datasets = []
     for i in range(steps):
-        if i==0:
+        if i == 0:
             data = dask_input
         else:
-            data = dask_input[...,::2,::2]
+            data = dask_input[..., ::2, ::2]
 
         try:
             data = data.rechunk()
             print(data.chunksize, data.shape, data.npartitions)
             if dry_run:
-                print(f'dry-run `to save to` {os.path.join(baseurl, str(i))}')
+                print(f"dry-run `to save to` {os.path.join(baseurl, str(i))}")
                 continue
-            
-            data.to_zarr(ppp:=os.path.join(baseurl, str(i)))
-            print(f'saved {ppp}')
+
+            data.to_zarr(ppp := os.path.join(baseurl, str(i)))
+            print(f"saved {ppp}")
             datasets.append({"path": str(i)})
-            grp.attrs['multiscales'] = {
+            grp.attrs["multiscales"] = {
                 "multiscales": [
                     {
                         "datasets": datasets,
@@ -39,16 +41,13 @@ def to_zarr(
                         "type": "nd2",
                         "channel_axis": channel_axis,
                         "version": "0.1",
-                        **kwargs
+                        **kwargs,
                     },
-
                 ]
             }
             dask_input = da.from_zarr(ppp)
         except Exception as e:
             print(e.args)
             raise e
-        
-    return baseurl
-    
 
+    return baseurl
